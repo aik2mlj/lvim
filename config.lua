335808ac03a8faf5cf335798a0affc0a36e70ff2
aik2mlj <aik2mlj@gmail.com>
2021-12-01 15:28:47 +0800

    add vim.opts, add latex configs, change tab behavior

--- config.lua
+++ config.lua
@@ -22,54 +22,64 @@ lvim.keys.insert_mode["<C-s>"] = "<ESC>:w<CR>"
 -- lvim.keys.normal_mode["<C-q>"] = ":q!<CR>"
 -- lvim.keys.insert_mode["<C-q>"] = "<ESC>:q!<CR>"
 
+vim.opt.expandtab = false
+vim.opt.wrap = true
+vim.opt.tw = 0
+vim.opt.virtualedit = "block"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.indentexpr = ""
+vim.opt.confirm = true
+
 -- My vim keymappings
 lvim.lsp.buffer_mappings.normal_mode["K"] = nil
 vim.cmd([[
-  map Q gq
-  noremap Y y$
-  nnoremap < <<
-  nnoremap > >>
-  nnoremap U <C-r>
+	map Q gq
+	noremap Y y$
+	nnoremap < <<
+	nnoremap > >>
+	nnoremap U <C-r>
 ]])
 vim.cmd([[
-  noremap <silent> H ^
-  noremap <silent> L $
-  noremap <silent> J 5j
-  noremap <silent> K 5k
+	noremap <silent> H ^
+	noremap <silent> L $
+	noremap <silent> J 5j
+	noremap <silent> K 5k
 ]])
 vim.cmd([[
-  noremap <C-f> <C-w>w
-  noremap s <nop>
-  noremap ss :vsplit<CR>
-  noremap sd :split<CR>
-  noremap sk :set nosplitbelow<CR>:split<CR>:set splitbelow<CR>
-  noremap sj :set splitbelow<CR>:split<CR>
-  noremap sh :set nosplitright<CR>:vsplit<CR>:set splitright<CR>
-  noremap sl :set splitright<CR>:vsplit<CR>
-  noremap so <C-w>o
-  noremap su <C-w>t<C-w>K
-  noremap sv <C-w>t<C-w>H
+	noremap <C-f> <C-w>w
+	noremap s <nop>
+	noremap ss :vsplit<CR>
+	noremap sd :split<CR>
+	noremap sk :set nosplitbelow<CR>:split<CR>:set splitbelow<CR>
+	noremap sj :set splitbelow<CR>:split<CR>
+	noremap sh :set nosplitright<CR>:vsplit<CR>:set splitright<CR>
+	noremap sl :set splitright<CR>:vsplit<CR>
+	noremap so <C-w>o
+	noremap su <C-w>t<C-w>K
+	noremap sv <C-w>t<C-w>H
 ]])
 vim.cmd([[
-  nnoremap <silent> <M-h> :bp<CR>
-  nnoremap <silent> <M-l> :bn<CR>
-  nnoremap <silent> <M-p> :b#<CR>
-  nnoremap <silent> <M-n> :enew<CR>
-  nnoremap <silent> <M-q> :bd<CR>
-  nnoremap <silent> <M-Q> :bd!<CR>
-  inoremap <silent> <M-h> <ESC>:bp<CR>
-  inoremap <silent> <M-l> <ESC>:bn<CR>
-  inoremap <silent> <M-p> <ESC>:b#<CR>
-  inoremap <silent> <M-n> <ESC>:enew<CR>
-  inoremap <silent> <M-q> <ESC>:bd<CR>
-  inoremap <silent> <M-Q> <ESC>:bd!<CR>
-  noremap <C-n> :vnew<CR>
+	nnoremap <silent> <M-h> :bp<CR>
+	nnoremap <silent> <M-l> :bn<CR>
+	nnoremap <silent> <M-p> :b#<CR>
+	nnoremap <silent> <M-n> :enew<CR>
+	nnoremap <silent> <M-q> :bd<CR>
+	nnoremap <silent> <M-Q> :bd!<CR>
+	inoremap <silent> <M-h> <ESC>:bp<CR>
+	inoremap <silent> <M-l> <ESC>:bn<CR>
+	inoremap <silent> <M-p> <ESC>:b#<CR>
+	inoremap <silent> <M-n> <ESC>:enew<CR>
+	inoremap <silent> <M-q> <ESC>:bd<CR>
+	inoremap <silent> <M-Q> <ESC>:bd!<CR>
+	noremap <C-n> :vnew<CR>
 ]])
 vim.cmd([[
-  nnoremap <M-J> :t .<CR>
-  nnoremap <M-K> :t .-1<CR>
-  xnoremap <M-J> :t'><CR>gv
-  xnoremap <M-K> :t-1<CR>gv
+	nnoremap <M-J> :t .<CR>
+	nnoremap <M-K> :t .-1<CR>
+	xnoremap <M-J> :t'><CR>gv
+	xnoremap <M-K> :t-1<CR>gv
 ]])
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
@@ -93,7 +103,6 @@ lvim.builtin.telescope.defaults.mappings = {
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["q"] = { "<cmd>q<CR>", "Quit" }
 lvim.builtin.which_key.mappings["Q"] = { "<cmd>q!<CR>", "Force Quit" }
-lvim.builtin.which_key.mappings["c"] = nil
 lvim.builtin.which_key.mappings["j"] = { "J", "Join Lines" }
 lvim.builtin.which_key.vmappings["j"] = { "J", "Join Lines" }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -123,6 +132,7 @@ lvim.builtin.treesitter.ensure_installed = {
 	"fish",
 	"javascript",
 	"json",
+	"latex",
 	"lua",
 	"python",
 	"typescript",
@@ -137,8 +147,9 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
 
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
+---@usage disable automatic installation of servers
+lvim.lsp.automatic_servers_installation = false
+lvim.lsp.diagnostics.virtual_text = false
 
 -- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
 -- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
@@ -152,7 +163,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
 --   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
+--	 vim.api.nvim_buf_set_option(bufnr, ...)
 --   end
 --   --Enable completion triggered by <c-x><c-o>
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -164,12 +175,12 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- or if you need something more advanced
 -- lvim.lsp.null_ls.setup.root_dir = function(fname)
 --   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
+--	 return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--	   or require("lspconfig/util").path.dirname(fname)
 --   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--	 return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
 --   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--	 return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
 --   end
 -- end
 
@@ -193,15 +204,15 @@ formatters.setup({
 -- linters.setup {
 --   { exe = "flake8", filetypes = { "python" } },
 --   {
---     exe = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     args = { "--severity", "warning" },
+--	 exe = "shellcheck",
+--	 ---@usage arguments to pass to the formatter
+--	 -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--	 args = { "--severity", "warning" },
 --   },
 --   {
---     exe = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
+--	 exe = "codespell",
+--	 ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--	 filetypes = { "javascript", "python" },
 --   },
 -- }
 
@@ -212,7 +223,23 @@ lvim.plugins = {
 		"folke/trouble.nvim",
 		cmd = "TroubleToggle",
 	},
+	-- { "lervag/vimtex" },
 }
+-- vim.g["tex_flavor"] = "latex"
+-- vim.g["vimtex_quickfix_mode"] = 0
+-- vim.g["vimtex_view_general_viewer"] = "/mnt/c/Users/ASUS/AppData/Local/SumatraPDF/SumatraPDF.exe"
+-- vim.g["vimtex_view_general_options"] = "-reuse-instance -forward-search @tex @line @pdf"
+-- vim.g["vimtex_view_general_options_latexmk"] = "-reuse-instance"
+-- * If in linux, use the following lines instead.
+-- vim.g["vimtex_view_general_viewer"] = "zathura"
+-- vim.g["vimtex_view_method"] = "zathura"
+-- vim.g["vimtex_compiler_progname"] = "nvr"
+local autoPair = require("nvim-autopairs")
+autoPair.setup({
+	disable_filetype = { "TelescopePrompt", "tex" },
+})
+autoPair.remove_rule("$")
+autoPair.disable()
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
